3579b401996288b9dcc1b93b8719535916f4c8e5
lincklopescarol <lincklopescarol@gmail.com>
2023-11-13 20:48:37 -0300

    refatorando as uris da api

--- to-do-service/src/main/java/com/example/todoservice/controller/TarefaController.java
+++ to-do-service/src/main/java/com/example/todoservice/controller/TarefaController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/lista")
+@RequestMapping("/tarefas")
 public class TarefaController {
 
     @Autowired
