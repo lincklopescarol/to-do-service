75541f6415caf268cadf072ee26c753eae3d99bf
lincklopescarol <lincklopescarol@gmail.com>
2023-11-13 21:28:23 -0300

    adicionando ResponseEntity nos métodos

--- to-do-service/src/main/java/com/example/todoservice/controller/TarefaController.java
+++ to-do-service/src/main/java/com/example/todoservice/controller/TarefaController.java
@@ -3,6 +3,8 @@ package com.example.todoservice.controller;
 import com.example.todoservice.Tarefa;
 import com.example.todoservice.service.TarefaService; // Importe a classe de serviço.
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,38 +17,44 @@ public class TarefaController {
     private TarefaService tarefaService;
 
     @GetMapping
-    public List<Tarefa> listarTarefas() {
-        // chamando o serviço para listar as tarefas.
-        return tarefaService.listarTarefas();
+    public ResponseEntity<List<Tarefa>> listarTarefas() {
+        List<Tarefa> tarefas = tarefaService.listarTarefas();
+        if (!tarefas.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.OK).body(tarefas);
+        } else {
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body(tarefas);
+        }
     }
 
     @PostMapping("/adicionar")
-    public void adicionarTarefa(@RequestBody Tarefa tarefa) {
-        // chamando o serviço para adicionar a tarefa.
-        tarefaService.adicionarTarefa(tarefa);
+    public ResponseEntity<String> adicionarTarefa(@RequestBody Tarefa tarefa) {
+        try {
+            tarefaService.adicionarTarefa(tarefa);
+            return ResponseEntity.status(HttpStatus.CREATED).body("Tarefa adicionada.");
+        } catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao adicionar a tarefa: " + ex.getMessage());
+        }
     }
 
     @PutMapping("/editar/{idTarefa}")
-    public String editarTarefa(@PathVariable int idTarefa, @RequestBody Tarefa tarefaAtualizada) {
-        // chamando o serviço para editar a tarefa.
+    public ResponseEntity<String> editarTarefa(@PathVariable int idTarefa, @RequestBody Tarefa tarefaAtualizada) {
         String resultado = tarefaService.editarTarefa(idTarefa, tarefaAtualizada);
 
         if (resultado.equals("Tarefa editada.")) {
-            return resultado;
+            return ResponseEntity.status(HttpStatus.OK).body("Tarefa editada.");
         } else {
-            return "Tarefa não encontrada.";
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Tarefa não encontrada.");
         }
     }
 
     @DeleteMapping("/remover/{idTarefa}")
-    public String removerTarefa(@PathVariable int idTarefa) {
-        // chamando o serviço para remover a tarefa.
+    public ResponseEntity<String> removerTarefa(@PathVariable int idTarefa) {
         String resultado = tarefaService.removerTarefa(idTarefa);
 
         if (resultado.equals("Tarefa removida.")) {
-            return resultado;
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
         } else {
-            return "Tarefa não encontrada.";
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Tarefa não encontrada.");
         }
     }
 }
